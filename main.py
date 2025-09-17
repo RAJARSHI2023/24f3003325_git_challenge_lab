644e27436c62628f5375b07e6490101801b95ea0
RAJARSHI DAS <24f3003325@ds.study.iitm.ac.in>
2025-09-17 20:59:46 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    num1 = 10
+    num2 = 5
+    print(f"The sum of {num1} and {num2} is {add(num1, num2)}")
+    print(f"The difference of {num1} and {num2} is {subtract(num1, num2)}")
+    print(f"The product of {num1} and {num2} is {multiply(num1, num2)}")
+  
